51cbd547710e937b2c4775c0201165034110bd57
Vicent Argente <vicentargente@gmail.com>
2022-11-16 13:32:47 +0100

    Paquete extractores

--- ProyectoIEI/src/App.java
+++ ProyectoIEI/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Arriba españa");
+        System.out.println("Hello, World!");
     }
 }
