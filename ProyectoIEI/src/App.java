d2a58cad4010f54d5d1c1cc033e29fffd2d0ab0d
angelperezz21 <aperezmartin2001@gmail.com>
2022-11-23 14:22:09 +0100

    SACADITA DE MYSQL PIM PAM TRUCUTRUCU

--- ProyectoIEI/src/App.java
+++ ProyectoIEI/src/App.java
@@ -9,7 +9,8 @@ public class App {
         Statement sqlSt;
         String useSQL = new String("EXTRACCIÓN");
         String output;
-        ResultSet result;
+        ResultSet resultPrueba;
+        ResultSet resultTabla;
         
         try{
             Class.forName("org.mariadb.jdbc.Driver");
@@ -18,10 +19,15 @@ public class App {
             sqlSt = dbConnect.createStatement(); //Permite a SQL ser ejecutado
             
             sqlSt.executeQuery("use test");
-            result = sqlSt.executeQuery("select * from prueba");
+            resultPrueba = sqlSt.executeQuery("select * from prueba");
+            resultTabla = sqlSt.executeQuery("select * from valencia");
             
-            while(result.next()!=false){
-                output = result.getString("numero");
+            while(resultPrueba.next()!=false){
+                output = resultPrueba.getString("numero");
+                System.out.println(output);
+            }
+            while(resultTabla.next()!=false){
+                output = resultTabla.getString("jugador");
                 System.out.println(output);
             }
             //System.out.println(result.getString(dbURL));
