377407b7803cfdb7d8bf9c70e1b0af1acb3328e7
angelperezz21 <aperezmartin2001@gmail.com>
2022-11-23 14:12:59 +0100

    FUNCIONA LA BASE DE DATOS

--- ProyectoIEI/src/App.java
+++ ProyectoIEI/src/App.java
@@ -1,5 +1,36 @@
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("BIENVENIDO LIONEL");
+
+        Statement sqlSt;
+        String useSQL = new String("EXTRACCIÓN");
+        String output;
+        ResultSet result;
+        
+        try{
+            Class.forName("org.mariadb.jdbc.Driver");
+            String dbURL = "jdbc:mariadb://IEI-006-v0.dsicv.upv.es:3306";
+            Connection dbConnect = DriverManager.getConnection(dbURL, "root", "");
+            sqlSt = dbConnect.createStatement(); //Permite a SQL ser ejecutado
+            
+            sqlSt.executeQuery("use test");
+            result = sqlSt.executeQuery("select * from prueba");
+            
+            while(result.next()!=false){
+                output = result.getString("numero");
+                System.out.println(output);
+            }
+            //System.out.println(result.getString(dbURL));
+
+        }
+        catch(SQLException ex){
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE,null,ex);
+            System.out.println("SQL IS BAD!!" + ex.getMessage());
+
+        }
     }
 }
