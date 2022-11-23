eb94c856bd046a705838b9914e433ca75f297c76
Samuel Cantó Ortuño <scanort@upv.edu.es>
2022-11-23 14:30:48 +0100

    ExtractorEUS avances

--- ProyectoIEI/src/extractores/ExtractorEUS.java
+++ ProyectoIEI/src/extractores/ExtractorEUS.java
@@ -1,5 +1,37 @@
 package extractores;
 
+import java.util.ArrayList;
+import Entidades.Hospital;
+import Entidades.Localidad;
+import Entidades.Provincia;
+import org.json.*;
+
 public class ExtractorEUS {
-    
+    JSONArray arrayJson;
+    ArrayList<Hospital> arrayRespuesta = new ArrayList<Hospital>();
+
+    private Hospital[] convertir(){
+        String nombre = "";
+        String tipo = "";
+        String direccion = "";
+        int codigoPostal = 000;
+        double longitud = 0;
+        double latitud = 0;
+        int telefono = 0;
+        String descripcion = "";
+        Localidad localidad = null;
+        Provincia provincia = null;
+
+        Hospital hospitales[] = new Hospital[arrayJson.length()];
+
+        for(int i = 0; i < hospitales.length; i++){
+            JSONObject x = (JSONObject) arrayJson.get(i);
+
+            String tipus = (String)x.get("Tipodecentro");
+            if(tipus == "Hospital"){tipo = "Hospital";}
+        }
+        return hospitales;
+    }
+
+
 }
