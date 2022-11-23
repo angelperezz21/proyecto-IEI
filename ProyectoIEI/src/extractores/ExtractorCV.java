cd6b53245cc6c8daba235413e3c3c5fd13e53321
Miguel Ángel Linares Martínez <malinmar@upv.edu.es>
2022-11-23 14:06:21 +0100

    ExtractorCV ejemplo

--- ProyectoIEI/src/extractores/ExtractorCV.java
+++ ProyectoIEI/src/extractores/ExtractorCV.java
@@ -1,5 +1,48 @@
 package extractores;
 
+import org.json.*;
+
+import Entidades.Hospital;
+import Entidades.Localidad;
+import Entidades.Provincia;
+
 public class ExtractorCV {
-    
+    JSONArray arrayJson;
+    public ExtractorCV(JSONArray arrayJson){
+        this.arrayJson = arrayJson;
+    }
+
+    public Hospital[] convertir(){
+        
+        String nombre = "";
+        String tipo = "";
+        String direccion = "";
+        int codigoPostal = 000;
+        double longitud = 0;
+        double latitud = 0;
+        int telefono = 0;
+        String descripcion = "";
+        Localidad localidad = null;
+        Provincia provincia = null;
+
+        Hospital hospitales[] = new Hospital[arrayJson.length()];
+
+        for(int i = 0; i < arrayJson.length(); i++){
+            JSONObject x = (JSONObject) arrayJson.get(i);
+
+            String tipus = (String)x.get("Tipus_centre");
+            if(tipus == "HOSPITALES DE MEDIA Y LARGA DISTANCIA"
+            || tipus == "HOSPITALES DE SALUD MENTAL Y TRATAMIENTO DE TOXICOMANÍAS"
+            || tipus == "HOSPITALES ESPECIALIZADOS"
+            || tipus == "HOSPITALES GENERALES"){
+                tipo = "Hospital";
+            }
+
+            Hospital hospital = new Hospital(nombre, tipo, direccion, codigoPostal, longitud, latitud, telefono, descripcion, localidad, provincia);
+            hospitales[i] = hospital;
+        }
+
+        return hospitales;
+        
+    }
 }
