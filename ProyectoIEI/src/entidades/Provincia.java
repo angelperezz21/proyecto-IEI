6e51c0e233e1fb3beb46515fa489f936b5a99d0f
Iván Piqueras Cerdán <ipiqcer@upv.edu.es>
2022-11-30 14:28:25 +0100

    guardate

--- ProyectoIEI/src/entidades/Provincia.java
+++ ProyectoIEI/src/entidades/Provincia.java
@@ -1,11 +1,28 @@
 package entidades;
 
 public class Provincia {
+    private int ID;
     private int codigo;
     private String nombre;
 
     public Provincia(int codigo, String nombre){
         this.codigo = codigo;
         this.nombre = nombre;
+    }
+
+
+    public int getID(){
+        return this.ID;
+    }
+
+    public int getCodigo(){
+        return this.codigo;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setID(int id){
+        this.ID = id;
     }
 }
