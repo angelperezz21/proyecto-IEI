197adc3e1709f1c716743ee7848695bc995b7a0b
Iván Piqueras Cerdán <ipiqcer@upv.edu.es>
2022-11-30 14:28:04 +0100

    alla que voy

--- ProyectoIEI/src/Crud.java
+++ ProyectoIEI/src/Crud.java
@@ -1,5 +1,5 @@
 import java.sql.*;
-
+import entidades.Provincia;
 public class Crud {
 
     private Connection conn = null;
@@ -15,5 +15,26 @@ public class Crud {
         String dbURL = "jdbc:mariadb://IEI-006-v0.dsicv.upv.es:3306";
         conn = DriverManager.getConnection(dbURL, "root", "");
         sqlSt = conn.createStatement(); //Permite a SQL ser ejecutado
+
+        
 	}
+
+    public void createProvincia(Provincia provincia) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement(
+                "insert into Provincia (Codigo, Nombre) values (?,?)'",
+                Statement.RETURN_GENERATED_KEYS);
+    
+        stmt.setString(1, Integer.toString(provincia.getCodigo()));
+        stmt.setString(2, provincia.getNombre());
+
+    
+        stmt.executeUpdate();
+        
+        ResultSet rs = stmt.getGeneratedKeys();
+        
+        if (rs.next()) {
+            provincia.setID(rs.getInt(1));
+        }
+        
+    }
 }
