866afecc6ae3e6b6a3378c72c5269dd89a82173d
angelperezz21 <aperezmartin2001@gmail.com>
2022-11-30 14:17:00 +0100

    Push CRUD SQL

--- ProyectoIEI/src/Crud.java
+++ ProyectoIEI/src/Crud.java
@@ -1,3 +1,19 @@
+import java.sql.*;
+
 public class Crud {
-    
+
+    private Connection conn = null;
+	Statement sqlSt;
+
+	public Crud() throws SQLException {
+		try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        String dbURL = "jdbc:mariadb://IEI-006-v0.dsicv.upv.es:3306";
+        conn = DriverManager.getConnection(dbURL, "root", "");
+        sqlSt = conn.createStatement(); //Permite a SQL ser ejecutado
+	}
 }
